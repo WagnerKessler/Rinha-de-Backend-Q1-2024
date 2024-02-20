cb891ae0bf06b4471ede4343c1ca8fb950445049
Wagner Kessler <wagner@kessler.dev>
2024-02-19 23:04:33 -0300

    Removed controller files, created services, organized project.

--- Dockerfile
+++ Dockerfile
@@ -8,17 +8,17 @@ EXPOSE 8080
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 ARG BUILD_CONFIGURATION=Release
 WORKDIR /src
-COPY ["mvc-api.csproj", "."]
-RUN dotnet restore "./mvc-api.csproj"
+COPY ["minimal-api.csproj", "."]
+RUN dotnet restore "./minimal-api.csproj"
 COPY . .
 WORKDIR "/src/."
-RUN dotnet build "./mvc-api.csproj" -c $BUILD_CONFIGURATION -o /app/build
+RUN dotnet build "./minimal-api.csproj" -c $BUILD_CONFIGURATION -o /app/build
 
 FROM build AS publish
 ARG BUILD_CONFIGURATION=Release
-RUN dotnet publish "./mvc-api.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+RUN dotnet publish "./minimal-api.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=true 
 
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
-ENTRYPOINT ["dotnet", "mvc-api.dll"]
+ENTRYPOINT ["dotnet", "minimal-api.dll"]
---
+++ Services/CustomerService.cs
@@ -0,0 +1,113 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+using Rinha_de_Backend_Q1_2024.Models;
+
+namespace Rinha_de_Backend_Q1_2024.Services
+{
+    public interface ICustomerService
+    {
+        Task<Customer?> GetCustomerByIdAsync(int id);
+        Task<List<Customer>> GetAllCustomersAsync();
+        Task<IResult> HandleCustomerCreationAsync(Customer newCustomer);
+    }
+
+    public class CustomerService : ICustomerService
+    {
+        private readonly NpgsqlConnection _connection;
+
+        public CustomerService(NpgsqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        // Retrieve a particular customer based on their ID
+        public async Task<Customer?> GetCustomerByIdAsync(int id)
+        {
+            var commandText = "SELECT * FROM public.\"Customers\" WHERE \"Id\" = @Id";
+            var parameters = new NpgsqlParameter("@Id", NpgsqlDbType.Integer) { Value = id };
+
+            using (var command = new NpgsqlCommand(commandText, _connection))
+            {
+                command.Parameters.Add(parameters);
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    if (await reader.ReadAsync())
+                    {
+                        return new Customer
+                        {
+                            Id = reader.GetInt32(0),
+                            Limit = reader.GetInt32(1),
+                            Balance = reader.GetInt32(2)
+                        };
+                    }
+                }
+            }
+            return null;
+        }
+
+        // Retrieve all customers
+        public async Task<List<Customer>> GetAllCustomersAsync()
+        {
+            var commandText = "SELECT * FROM public.\"Customers\"";
+            var customers = new List<Customer>();
+
+            using (var command = new NpgsqlCommand(commandText, _connection))
+            using (var reader = await command.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                {
+                    var customer = new Customer
+                    {
+                        Id = reader.GetInt32(0),
+                        Limit = reader.GetInt32(1),
+                        Balance = reader.GetInt32(2)
+                    };
+
+                    customers.Add(customer);
+                }
+            }
+
+            return customers;
+        }
+
+        // Create a new customer
+        public async Task<IResult> HandleCustomerCreationAsync(Customer newCustomer)
+        {
+            if (newCustomer == null)
+            {
+                return Results.BadRequest("Invalid input");
+            }
+
+            var commandText = "INSERT INTO public.\"Customers\" (\"Id\", \"Limit\", \"Balance\") VALUES (@Id, @Limit, @Balance) RETURNING *";
+
+            using (var command = new NpgsqlCommand(commandText, _connection))
+            {
+                command.Parameters.AddWithValue("@Id", newCustomer.Id);
+                command.Parameters.AddWithValue("@Limit", newCustomer.Limit);
+                command.Parameters.AddWithValue("@Balance", newCustomer.Balance);
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    if (await reader.ReadAsync())
+                    {
+                        var createdCustomer = new Customer
+                        {
+                            Id = reader.GetInt32(0),
+                            Limit = reader.GetInt32(1),
+                            Balance = reader.GetInt32(2)
+                        };
+
+                        return Results.Created($"/clientes/{createdCustomer.Id}", createdCustomer);
+                    }
+                }
+            }
+
+            return Results.BadRequest("Failed to create customer");
+        }
+
+
+
+
+    }
+}
